804fae0b14f5f6bf36b25466ef2a10219a222d53
ddeshayes <dayann.deshayes@scub.net>
2025-01-22 12:10:50 +0100

    Initial commit

---
+++ src/main/java/com/m2i/app_publication_annonce/AppPublicationAnnonceApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.app_publication_annonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPublicationAnnonceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPublicationAnnonceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2i/app_publication_annonce/AppPublicationAnnonceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.app_publication_annonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPublicationAnnonceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
